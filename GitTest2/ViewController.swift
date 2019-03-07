4e3915a7d1439b0b3274648455a205a0b6d41e43
D7703_22 <d7703_22@promise.local>
2019-03-07 17:09:39 +0900

    Initial Commit

---
+++ GitTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest2
+//
+//  Created by D7703_22 on 2019. 3. 7..
+//  Copyright © 2019년 fb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
